20b8c224cf0c32c0026af3c4ee24966c2e1246fd
Альберт Астадурьян <albertastaduran@Alberts-MacBook-Air.local>
2020-11-07 21:40:43 +0300

    NEW: create zoopark project

---
+++ animal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('products/', views.products, name='product'),
+#     path('contact/', views.contact, name='contact'),
+#     path('', views.main, name='main')
+# ]
